a4c4e93dec215318cb250ae76e2434f35160cabf
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-02-18 12:42:29 +0530

    logo added

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+import download from "./download.png";
+import logo from "./logo.svg";
+import logo1 from "./logo1.png";
+import preview from "./preview.png";
+
+export { download, logo, logo1, preview };

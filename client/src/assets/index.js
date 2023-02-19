d3e9dcd5eeb0f0e96923e7da1906f170bb9a9d71
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-02-19 11:09:14 +0530

    logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,6 @@
 import download from "./download.png";
 import logo from "./logo1.png";
 import preview from "./preview.png";
+import loading from "./loading.svg";
 
-export { download, logo, preview };
+export { download, logo, preview, loading };

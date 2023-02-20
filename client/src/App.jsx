7cafdce544db9b483c7eeaf4ba4e7cda7ef34074
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-02-20 11:05:10 +0530

    root component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { CreatePost, Home } from "./pages";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { logo } from "./assets";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Toaster position="top-right" />
 			<header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 				<Link to="/">
 					<img src={logo} alt="logo" className="w-28 object-contain" />
 				</Link>
 				<Link
-					to="/create-post"
+					to="/create"
 					className="font-inter font-medium bg-[#0C0D0D] text-white px-4 py-2 rounded-md"
 				>
 					Create

0726a223785c94f0dc14f3f10422a6809c153e2a
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-02-20 21:12:04 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import connectDB from "./src/database/connect.js";
+import indexRoute from "./src/routes/index.js";
+
+connectDB;
 
 dotenv.config();
+// console.log(dotenv.config());
 // Enables env files to be saved in .env files.
 
 const port = normalizePort(process.env.PORT || "5000");
@@ -12,12 +17,21 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
+
+// root route
+app.use("/api/v1", indexRoute);
 app.get("/", async (req, res) => {
 	res.send("Hello, Pixify!");
 });
 
 const startServer = async () => {
-	app.listen(port, () => console.log(`server started on port:${port}`));
+	try {
+		connectDB(process.env.DATABASE_URL);
+		app.listen(port, () => console.log(`server started on port:${port}`));
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 startServer();
